327591be1805f2d6b1444f41ef1da23c29608ce5
Anubhab Sen <anubhabsen@gmail.com>
2017-09-02 22:40:41 +0530

    code to colorise

---
+++ rgb.m
@@ -0,0 +1,32 @@
+img = imread('7.tif');
+h = floor(size(img, 1) / 3);
+width = round(size(img, 2));
+img1 = img(1 : h, :);
+img2 = img(h + 1 : 2 * h, :);
+img3 = img(2 * h + 1: 3 * h, :);
+
+fixed = img1;
+moving = img2;
+
+[optimizer, metric] = imregconfig('multimodal');
+tform = imregtform(moving, fixed, 'translation', optimizer, metric);
+movingRegistered1 = imwarp(moving,tform,'OutputView',imref2d(size(fixed)));
+
+moving2 = img3;
+[optimizer, metric] = imregconfig('multimodal');
+tform = imregtform(moving2, fixed, 'translation', optimizer, metric);
+movingRegistered2 = imwarp(moving2,tform,'OutputView',imref2d(size(fixed)));
+
+im = zeros(h, width, 3);
+im(:, :, 1) = movingRegistered2;
+im(:, :, 2) = movingRegistered1;
+im(:, :, 3) = img1;
+
+% figure
+% imshow(movingRegistered2);
+% figure
+% imshow(movingRegistered1);
+% figure
+% imshow(img1);
+figure
+imshow(uint16(im));
